8e72dcf85a24964e4bb821f3c67af2966ab9c2bc
Phil Dyer <phildyer@protonmail.com>
2021-07-08 15:08:30 +1000

    nix_shell_pin.sh: prevent nix-shell getting gc'd

---
+++ nix_shell_pin.sh
@@ -0,0 +1,11 @@
+#! /bin/sh -eu
+
+# make gcroot for each dependency of shell and run nix-shell
+#usage
+# nix_shell_pin.sh SHELL_FILE [gcpath]
+gcroot=${2:-$PWD}
+
+mkdir -p $gcroot
+nix-instantiate $1 --indirect --add-root $gcroot/.gcroots/shell.drv
+nix-store --indirect --add-root $gcroot/.gcroots/shell.dep --realise $(nix-store --query --references $gcroot/.gcroots/shell.drv)
+exec nix-shell $(readlink $gcroot/.gcroots/shell.drv) --command "echo 'Entering zsh';zsh"
